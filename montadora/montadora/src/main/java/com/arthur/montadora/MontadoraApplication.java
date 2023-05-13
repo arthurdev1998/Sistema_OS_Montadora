a11419b36a244ed108d487c26fbbbbccc6722cff
Arthur Mendes <arthurdev1998@gmail.com>
2023-05-13 18:05:08 -0300

    Inicio_project

---
+++ montadora/montadora/src/main/java/com/arthur/montadora/MontadoraApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.montadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontadoraApplication.class, args);
+	}
+
+}
